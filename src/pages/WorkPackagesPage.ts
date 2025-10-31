b686f08585b21b1602e37c17f718bec8fb254956
rene-ad <rene.diaz@assuresoftbolivia.com>
2025-10-31 03:18:59 -0400

    Feature: create worl package from table form

--- src/pages/WorkPackagesPage.ts
+++ src/pages/WorkPackagesPage.ts
@@ -25,4 +25,39 @@ export class WorkPackagesPage {
     return errorMessageToaster;
   }
 
+  async clickCreateNewWorkPackageButton(){
+    await this.page.locator(`button:has-text("${workPackagesLocators.createNewWorkPackageButton}")`).click();
+  }
+
+  async waitForNewRowInTable(timeWaiter:number){
+    await this.page.locator(workPackagesLocators.newRowInTable).waitFor({ state: "visible", timeout: timeWaiter });
+  }
+
+  async fillTaskNameInputFromTable(name:string){
+    await this.page.locator(workPackagesLocators.taskNameInputFromTable).fill(name);
+  }
+
+  async clickSelectPackageTypeFromTable(){
+    await this.page.locator(workPackagesLocators.packageTypeInputFromTable).click();
+  }
+
+  async waitForPackageTypesContainerFromTable(timeWaiter:number){
+    await this.page.locator(workPackagesLocators.packageTypesContainerFromTable).waitFor({ state: "visible", timeout: timeWaiter });
+  }
+
+  async clickTaskTypeOptionFromTable(){
+    await this.page.locator(workPackagesLocators.taskTypeOptionFromTable).click();
+  }
+
+  async clickSelectProjectFromTable(){
+    await this.page.locator(workPackagesLocators.projectInputFromTable).click();
+  }
+
+  async waitForProjectsContainerFromTable(timeWaiter:number){
+    await this.page.getByRole('listbox', { name: workPackagesLocators.projectContainerFromTable }).waitFor({ state: "visible", timeout: timeWaiter });
+  }
+
+  async selectProjectOptionFromTable(){
+    await this.page.getByText(workPackagesLocators.workPackageModuleProjectOption).click();
+  }
 }
